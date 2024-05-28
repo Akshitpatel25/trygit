3c41bdf1688e88af2689b4855b11de83c269e04b
Akshitpatel25 <patelakshit1225@gmail.com>
2024-05-28 12:34:47 +0530

    eventPropagation

--- index.js
+++ index.js
@@ -1,126 +1,133 @@
-let newDate = new Date();
+// let newDate = new Date();
 
-let hours = newDate.getHours();
-let minutes = newDate.getMinutes();
-let seconds = newDate.getSeconds();
+// let hours = newDate.getHours();
+// let minutes = newDate.getMinutes();
+// let seconds = newDate.getSeconds();
 
-// Format the time with leading zeros if necessary
-let formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
-  .toString()
-  .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+// // Format the time with leading zeros if necessary
+// let formattedTime = `${hours.toString().padStart(2, "0")}:${minutes
+//   .toString()
+//   .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
-// console.log(formattedTime);
+// // console.log(formattedTime);
 
-// let obj1 = {
-//     name: 'John',
-//     age: 30,
-// }
+// // let obj1 = {
+// //     name: 'John',
+// //     age: 30,
+// // }
 
-// let obj2 = {
-//     nam: 'Johny',
-//     ag: 33,
-// }
+// // let obj2 = {
+// //     nam: 'Johny',
+// //     ag: 33,
+// // }
 
-// let allobj = Object.keys({...obj1, ...obj2})
+// // let allobj = Object.keys({...obj1, ...obj2})
 
-// let num1 = 2;
-// let num2 = 3;
-// function mysocre() {
-//     return num1 + num2
-// }
+// // let num1 = 2;
+// // let num2 = 3;
+// // function mysocre() {
+// //     return num1 + num2
+// // }
 
-// let result = mysocre()
+// // let result = mysocre()
 
-// let obj = {
-//     name : "akshit",
-//     age: 22,
+// // let obj = {
+// //     name : "akshit",
+// //     age: 22,
 
-//     myobj: function() {
-//         console.log(`${this.name} object masa aaya hai`);
-//     },
-// }
-// obj.myobj();
+// //     myobj: function() {
+// //         console.log(`${this.name} object masa aaya hai`);
+// //     },
+// // }
+// // obj.myobj();
 
-// (function chai(){
-//     console.log("hello world");
-// })()
+// // (function chai(){
+// //     console.log("hello world");
+// // })()
 
-// ((name) => {
-//     console.log(`hello ${name.ag1} arrow function`);
-// })({na1: "akshit", ag1:22})
+// // ((name) => {
+// //     console.log(`hello ${name.ag1} arrow function`);
+// // })({na1: "akshit", ag1:22})
 
-// function myscore(...num1) {
-//     return num1
-// }
+// // function myscore(...num1) {
+// //     return num1
+// // }
 
-// console.log(myscore(100,200,300));
+// // console.log(myscore(100,200,300));
 
-// const arr = [1,2,3,4,5,6,7]
+// // const arr = [1,2,3,4,5,6,7]
 
-// for (const key in arr) {
-//     console.log(`${arr[key]}`);
-// }
+// // for (const key in arr) {
+// //     console.log(`${arr[key]}`);
+// // }
 
-let arr = [
-  {
-    color: "purple",
-    type: "minivan",
-    price: 100,
-    registration: new Date("2012-02-03"),
-    capacity: 7,
-  },
-  {
-    color: "red",
-    type: "SUV",
-    registration: new Date("2012-12-23"),
-    capacity: 7,
-    price: 200,
-  },
-  {
-    color: "red",
-    type: "saden",
-    registration: new Date("2022-12-23"),
-    capacity: 4,
-    price: 300,
-  },
-  {
-    color: "blue",
-    type: "sports",
-    registration: new Date("2020-01-10"),
-    capacity: 2,
-    price: 400,
-  },
-];
+// let arr = [
+//   {
+//     color: "purple",
+//     type: "minivan",
+//     price: 100,
+//     registration: new Date("2012-02-03"),
+//     capacity: 7,
+//   },
+//   {
+//     color: "red",
+//     type: "SUV",
+//     registration: new Date("2012-12-23"),
+//     capacity: 7,
+//     price: 200,
+//   },
+//   {
+//     color: "red",
+//     type: "saden",
+//     registration: new Date("2022-12-23"),
+//     capacity: 4,
+//     price: 300,
+//   },
+//   {
+//     color: "blue",
+//     type: "sports",
+//     registration: new Date("2020-01-10"),
+//     capacity: 2,
+//     price: 400,
+//   },
+// ];
 
-// arr = arr.filter( (car) => {
-//     return car.type === "SUV" && car.color === "red"
-// })
-// arr = arr.filter( (car) => car.type === "saden" )
+// // arr = arr.filter( (car) => {
+// //     return car.type === "SUV" && car.color === "red"
+// // })
+// // arr = arr.filter( (car) => car.type === "saden" )
 
-// console.log(arr);
+// // console.log(arr);
 
-// const nums = [1,2,3,4]
+// // const nums = [1,2,3,4]
 
-// let no = nums.reduce( function (acc, curr) {
-//     console.log(`acc: ${acc} and curr: ${curr}`);
-//     return acc + curr
-// })
+// // let no = nums.reduce( function (acc, curr) {
+// //     console.log(`acc: ${acc} and curr: ${curr}`);
+// //     return acc + curr
+// // })
 
-// console.log(no)
+// // console.log(no)
 
-// let no = arr.reduce( (arr, curr) => arr + curr.price,0)
-// console.log(no)
+// // let no = arr.reduce( (arr, curr) => arr + curr.price,0)
+// // console.log(no)
 
 
-function getFee(isMember) {
-  return isMember ? '$2.00' : '$10.00';
-}
+// // function getFee(isMember) {
+// //   return isMember ? '$2.00' : '$10.00';
+// // }
 
-console.log(getFee(true));
-// Expected output: "$2.00"
+// // console.log(getFee(true));
+// // // Expected output: "$2.00"
 
-console.log(getFee(false));
-// Expected output: "$10.00"
+// // console.log(getFee(false));
+// // // Expected output: "$10.00"
 
-console.log(getFee(null));
-// Expected output: "$10.00"
+// // console.log(getFee(null));
+// // // Expected output: "$10.00"
+
+// const div = document.createElement('div')
+
+// div.className = "main";
+// div.innerHTML = "Hello World";
+// div.style.color = "red";
+// document.body.appendChild(div)
